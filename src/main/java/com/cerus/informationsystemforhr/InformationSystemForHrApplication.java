5c1abeb3b417af6d4ed250d4d2e75d17b9b98f48
Vlad Tastenov <vladtastenov@gmail.com>
2024-02-04 17:36:05 +0500

    Create a project

---
+++ src/main/java/com/cerus/informationsystemforhr/InformationSystemForHrApplication.java
@@ -0,0 +1,13 @@
+package com.cerus.informationsystemforhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationSystemForHrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InformationSystemForHrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cerus/informationsystemforhr/InformationSystemForHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cerus.informationsystemforhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationSystemForHrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
